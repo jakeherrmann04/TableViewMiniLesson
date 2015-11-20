7e7729bd2dc5615a7cdf4ae2f6633659a0c1140e
jakeherrmann04 <jake.herrmann04@gmail.com>
2015-11-20 10:48:32 -0700

    Finished lesson

--- TableViewMiniLesson/ViewController.swift
+++ TableViewMiniLesson/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cellID") as UITableViewCell?
+
+        cell?.textLabel?.text = daysOfWeek[indexPath.row]
+        
+        return cell!
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return daysOfWeek.count
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 135
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "detailSegue"{
+        
+            let detailViewController  = segue.destinationViewController as! DetailViewController
+            let indexPath : NSIndexPath! = tableView.indexPathForSelectedRow
+            detailViewController.titleString = daysOfWeek[indexPath.row]
+            
+
+
+        }
+        
+        
+    }
+    
+    
 
 }
 
